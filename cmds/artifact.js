1904d79209a7e087acbf1a6bf390eb35903b67dd
CsiPA0723 <csizipeti0723@gmail.com>
2019-12-11 20:55:11 +0100

    fixes again

--- cmds/artifact.js
+++ cmds/artifact.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-
+const { mode } = require('../config.json');
 const database = require('../database');
 
 /**
@@ -12,7 +12,7 @@ const database = require('../database');
 module.exports.run = async (bot, message, args) => {
     const currencyData = database.GetData("currency", message.author.id);
     var now = Date.now();
-    if(now >= currencyData.artifactClaimTime + database.config.HalfDayInMilliSeconds || message.author.id == bot.devId) {
+    if(now >= currencyData.artifactClaimTime + database.config.HalfDayInMilliSeconds || (message.author.id == bot.devId && mode == "development")) {
         var artifact = Artifact();
         currencyData.rub += artifact.rub;
         currencyData.artifactClaimTime = now;
@@ -26,41 +26,41 @@ module.exports.run = async (bot, message, args) => {
 
 function Artifact() {
     var outcomes = [{
+        good: false,
+        msg: "Azta! Ez igen értékes darab! 3000 RUB az értéke! Ezért az egész bár a te kontodra szórakozott! Sajnos bőven elitták az árát. **# RUB**",
+        rubMax: 350,
+        rubMin: 200,
+        chance: 10
+    },{
         good: true,
         msg: "Értékes képződményt találtál, amiért eladtad: **# RUB**",
         rubMax: 350,
         rubMin: 200,
-        chance: 20
+        chance: 10
     },{
         good: false,
         msg: "Majdnem ott hagytad a fogad, te radioaktív kutyatáp! A mocsári doktor mentett meg! Orvosi költség: **# RUB**",
         rubMax: 150,
         rubMin: 100,
-        chance: 30  
+        chance: 20  
     },{
         good: false,
         msg: "Értékes képződményt találtál, de a bárban el is ittad az árát… Sőt többet is **# RUB**",
         rubMax: 200,
         rubMin: 100,
-        chance: 30  
+        chance: 20  
     },{
         good: false,
         msg: "Ördög-gyám képződményt találtál… Inkább hagyd ott **# RUB**",
         rubMax: 0,
         rubMin: 0,
-        chance: 40 
+        chance: 20 
     },{
         good: false,
         msg: "A kutatásod során rátok támadtak, nem találtál semmit. Sőt, egy bicskád is odaveszett! **# RUB**",
         rubMax: 0,
         rubMin: 0,
-        chance: 40  
-    },{
-        good: false,
-        msg: "Azta! Ez igen értékes darab! 3000 RUB az értéke! Ezért az egész bár a te kontodra szórakozott! Sajnos bőven elitták az árát. **# RUB**",
-        rubMax: 350,
-        rubMin: 200,
-        chance: 20
+        chance: 20  
     }
 ];
 
@@ -71,14 +71,16 @@ function Artifact() {
 
     var rnd = Math.random();
     var acc = 0;
-    for (var i=0, o; o = outcomes[i]; i++) {
-        acc += o.chance / 100;
+    var i = 0;
+    while(rnd > acc && i < outcomes.length) {
+        acc += outcomes[i].chance / 100;
         if (rnd < acc) {
-            var rub = Math.floor(Math.random() * (o.rubMax - o.rubMin) + o.rubMin);
-            if(o.good) retruned.rub = rub;
+            var rub = Math.floor(Math.random() * (outcomes[i].rubMax - outcomes[i].rubMin) + outcomes[i].rubMin);
+            if(outcomes[i].good) retruned.rub = rub;
             else retruned.rub = -rub;
-            retruned.msg = o.msg.replace("#", `${retruned.rub}`);
+            retruned.msg = outcomes[i].msg.replace("#", `${retruned.rub}`);
         };
+        i++;
     }
 
     return retruned;
